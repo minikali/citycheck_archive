8d29696323cd2ff9df73bad3abff1755aeba8bb6
Remi LAK <remi.lak@new-immo-group.com>
2021-01-04 22:23:34 +0100

    fix banner

--- src/components/DraggableMarker/index.tsx
+++ src/components/DraggableMarker/index.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 
-// eslint-disable-next-line arrow-body-style
+const center = {
+  lat: 51.505,
+  lng: -0.09,
+}
+
 const DraggableMarker = () => {
+  const [draggable, setDraggable] = useState(false);
+  const [position, setPosition] = useState(center);
+  const markerRef = useRef(null);
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          setPosition(marker.getLatLng());
+        }
+      },
+    }),
+    []
+  );
+  const toggleDraggable = useCallback(() => {
+    setDraggable((d) => !d);
+  }, []);
+
   return (
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup.
-        <br />
-        Easily customizable.
+    <Marker
+      draggable={draggable}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    >
+      <Popup minWidth={90}>
+        <span onClick={toggleDraggable}>
+          {draggable
+            ? 'Marker is draggable'
+            : 'Click here to make marker draggable'}
+        </span>
       </Popup>
     </Marker>
   );
