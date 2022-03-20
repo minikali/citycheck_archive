fc720d5a71b4be609b3b006aa32d4ad9bb02fdcb
Remi LAK <remilak11@gmail.com>
2022-03-20 20:29:46 +0100

    ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'citycheck',
+      script: 'npm',
+      args: 'start -- -p 3000',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
